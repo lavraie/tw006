82705cb79faa8612e62ac6ce8d8f86c839c6be06
lavraie <63726257+lavraie@users.noreply.github.com>
2020-05-19 14:19:11 +0200

    1

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ const urls = [
 ];
 // use map() to perform a fetch and handle the response for each url
 var data5 = [];
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
 async function promiseAll() {
     await Promise.all(urls.map(url =>
         fetch(url)
