be4f507057a86450c019fdef23735234a1d9a383
lavraie <63726257+lavraie@users.noreply.github.com>
2020-05-23 12:00:02 +0200

    1

--- main.js
+++ main.js
@@ -84,3 +84,27 @@ function getData() {
     promiseAll();
 }
 getData();
+
+function myFunction() {
+    let url = 'https://www.overpass-api.de/api/interpreter?data=[out:json];node[amenity=restaurant](46.1138,-1.1630,46.1925,-1.1511);out meta;';
+    var bounds = mymap.getBounds();
+    console.log(bounds);
+    fetch(url)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
